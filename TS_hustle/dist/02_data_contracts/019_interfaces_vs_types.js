0612e772d85dd6e313aef1c08c649222ff48b11c
Afnan Khan <khanafnan1108@gmail.com>
2026-07-03 00:48:57 +0530

    build: remove typescript roadmap file

--- TS_hustle/dist/02_data_contracts/019_interfaces_vs_types.js
+++
@@ -1,9 +0,0 @@
-// Interface -> Object Shapes that you expects to extends
-//  Supports declaration Merging
-//  Types -> More general -> Objects, Unions, Intersections, Functions
-// Can not be reopened -> No merging
-const BoxDemo = { width: 10, height: 1 };
-export {};
-/* type Bag = { size: number };
-type Bag = { color: string }; */
-// Can't redeclare type aliases name
